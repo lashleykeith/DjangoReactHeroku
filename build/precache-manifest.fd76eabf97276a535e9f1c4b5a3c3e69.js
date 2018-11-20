9363829956ac9d69bd49c464cc1f5b022aec011c
Keith Lashley <lashleykeith@gmail.com>
2018-11-21 00:22:00 +0900

    Deployment
    This is ready for deployment but I have trouble adding new post to it.

---
+++ build/precache-manifest.fd76eabf97276a535e9f1c4b5a3c3e69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a9b55de6a4f1bf7c790",
+    "url": "/static/js/main.5a9b55de.chunk.js"
+  },
+  {
+    "revision": "11866f3ecb27cf70ae12",
+    "url": "/static/js/1.11866f3e.chunk.js"
+  },
+  {
+    "revision": "11866f3ecb27cf70ae12",
+    "url": "/static/css/1.4bc2094e.chunk.css"
+  },
+  {
+    "revision": "1f5d603564a65eafa1e35fb7b6f0ba13",
+    "url": "/index.html"
+  }
+];
